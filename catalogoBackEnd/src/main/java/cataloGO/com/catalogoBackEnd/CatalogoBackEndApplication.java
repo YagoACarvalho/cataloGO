46753f295ce59244ab9b40c1d8ea2fd729b28b23
Yago <yagodevtech@gmail.com>
2025-08-28 19:05:32 -0300

    first commit

---
+++ catalogoBackEnd/src/main/java/cataloGO/com/catalogoBackEnd/CatalogoBackEndApplication.java
@@ -0,0 +1,13 @@
+package cataloGO.com.catalogoBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoBackEndApplication.class, args);
+	}
+
+}
---
+++ catalogoBackEnd/src/test/java/cataloGO/com/catalogoBackEnd/CatalogoBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package cataloGO.com.catalogoBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
